99899588b7d6b98452394118d6a77334ad667a72
luiz0217 <luiz.padletski@escolaoptimus.com>
2025-08-10 17:41:06 -0300

    api de produção com gambiarra

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
+  //baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
+  baseURL: "https://asgat.khoddex.com.br" + '/api',
 });
 
 api.interceptors.request.use((config) => {
